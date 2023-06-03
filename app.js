caabbc0f10cf8e7c00e1ff1aadbd1b8a0701f341
hauntedkeyboard <118305462+hauntedkeyboard@users.noreply.github.com>
2023-06-03 18:37:59 +0400

    add timer object for the sake of learning

--- app.js
+++ app.js
@@ -56,3 +56,43 @@ function makePetDiv() {
 };
 
 makePetDiv();
+
+
+function Stopwatch() {
+    let startTime, endTime, running;
+    let duration = 0;
+
+
+    this.start = function() {
+        if (running) {
+            throw new Error("Stopwatch has already started, you dummy");
+        } 
+
+        running = true;
+
+        startTime = new Date();
+
+    };
+
+    this.stop = function() {
+        if (!running) {
+            throw new Error("What are you even trying to stop?");
+        };
+
+        running = false;
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
+
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+
+    Object.defineProperty(this, "duration", { get: function() { return duration }});
+};
